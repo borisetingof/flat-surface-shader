1d658480481c0b832f9eeb346ef2d73bdb3abb4c
Boris Etingof <boris.etingof@reckon.com>
2018-05-22 15:05:11 +1000

    Add var declaration

--- deploy/fss.js
+++ deploy/fss.js
@@ -33,7 +33,7 @@
  * Defines the Flat Surface Shader namespace for all the awesomeness to exist upon.
  * @author Matthew Wagerfield
  */
-FSS = {
+var FSS = {
   FRONT  : 0,
   BACK   : 1,
   DOUBLE : 2,
